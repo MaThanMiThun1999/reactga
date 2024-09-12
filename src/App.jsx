4c36279ea66ea8270a47c27c9df999a1397ea6f7
TASDevelopers <thearcsoldevelopers@gmail.com>
2024-09-12 14:53:32 +0530

    Added ReactGA

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Title from "./components/home/Title";
+import ReactGA from 'react-ga4';
 
 export default function App() {
+
+  const mesurementId = 'G-L3W6R8DCDP';
+
+  ReactGA.initialize(mesurementId);
+
   return (
     <main>
       <h1>Hello, World!</h1>
