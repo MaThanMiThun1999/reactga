d1e10448cc07b69533ae59f49844e31f9ed22646
TASDevelopers <thearcsoldevelopers@gmail.com>
2024-09-12 14:40:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Title from "./components/home/Title";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Hello, World!</h1>
+      <p>This is my React App</p>
+      <p>Check it out!</p>
+      <Title/>
+    </main>
+  )
+}
+
